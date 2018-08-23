93dd56f39441265dc74c8e570ae8d9c440f7f976
BRJW <bjohnsonward@gmail.com>
2018-08-23 08:34:33 -0400

    Adding allure

--- RunTests.bat
+++ RunTests.bat
@@ -1,5 +1,6 @@
 docker container run --rm --network="host" -v "%cd%/Tests":/usr/src/app/Tests williamsault/protractor:latest
 docker stop allure
+docker run --rm -d --name=allure -p 8800:8800 -v "%cd%/Tests/allure-results:/allure-results" williamsault/allure
 timeout 4
 start chrome --new-window "http://localhost:8800"
 pause
