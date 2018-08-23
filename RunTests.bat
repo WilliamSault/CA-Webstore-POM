5ea6e236835acf1ff7d23a4044346de5f59b076d
BRJW <bjohnsonward@gmail.com>
2018-08-23 02:10:17 -0400

    Added scripts for windows

--- RunTests.bat
+++ RunTests.bat
@@ -1,3 +1,5 @@
-
-docker container run --rm --network="host" -v "%cd%/Tests:/user/src/app/Tests" williamsault/protractor:latest
-
+docker container run --rm --network="host" -v "%cd%/Tests":/usr/src/app/Tests williamsault/protractor:latest
+docker stop allure
+timeout 4
+start chrome --new-window "http://localhost:8800"
+pause
